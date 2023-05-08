c4094472dd01dfb9836a843393682bdaaeae3339
the-real-finnventor <71413483+the-real-finnventor@users.noreply.github.com>
2023-05-08 08:26:19 -0500

    Update log.php

--- log.php
+++ log.php
@@ -1,4 +1,15 @@
 <?php
-$data = $_SERVER['REMOTE_ADDR'] . ' ' . date('Y-m-d H:i:s') . "\n";
-file_put_contents('log.txt', $data, FILE_APPEND);
+  // Get the user data from Cloudflare's trace endpoint
+  $userData = file_get_contents('https://www.cloudflare.com/cdn-cgi/trace');
+
+  // Add a timestamp to the user data
+  $userData .= "\nTimestamp: " . date("Y-m-d H:i:s") . "\n\n";
+
+  // Append the user data to the log file
+  $logFile = fopen("log.txt", "a") or die("Unable to open file!");
+  fwrite($logFile, $userData);
+  fclose($logFile);
+
+  // Echo back the appended data for debugging purposes
+  echo $userData;
 ?>
